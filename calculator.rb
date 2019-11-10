e1e89821fe1f20f3bcb2dc623a36a38620a96f11
Alexander kagerer <kagerer19@gmail.com>
2019-11-10 18:56:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,3 +5,4 @@ sum = (first_number + second_number)
 sub = (first_number - second_number)
 difference = sub = (first_number - second_number)
 product = first_number * second_number
+quotient = first_number / second_number
